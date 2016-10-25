9099f7a3ff90b0ae184a837d3fb9e9d1094d45f3
Akira Kurogane <akira.kurogane@10gen.com>
2016-10-25 16:02:17 +1100

    Adding bifurcating search for approximate ts_start

--- ffc.c
+++ ffc.c
@@ -1,13 +1,73 @@
 #include <stdio.h>
 #include <string.h>
 #include <strings.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
 #include "ffc.h"
+
+void seek_start_ts(FILE* fstr, int verbose_flag, const char* filter_ts_start) {
+  off_t close_enough = 10 * 1024 * 1024; //the size we stop seeking, just scan all
+  off_t fpos = 0;
+  
+  struct stat st;
+  int st_status;
+  st_status = fstat(fileno(fstr), &st);
+  off_t file_sz = st.st_size;
+  off_t jump_sz = file_sz / 2;
+  fpos = jump_sz; //start in the middle of the file
+  fseek(fstr, fpos, SEEK_SET);
+
+  int c;
+  const size_t bufsize = 256 * 1024;
+  char buf[bufsize];
+  unsigned int line_ctr = 0;
+  unsigned int escape_line_no = 50; //how many lines to look through before giving up on finding a timestamped one
+
+  c = getc(fstr);
+  if (c == EOF)
+    return;
+
+  ungetc(c, fstr);
+  c = 0;
+
+  int dt_str_cmp;
+  while (jump_sz > close_enough) {
+    char* fgres = NULL;
+    while (fgres = fgets(buf, bufsize, fstr)) { //looking for first valid timestamped line
+      line_ctr++;
+      if (line_ctr >= escape_line_no) {
+        break;
+      }
+      if (buf[0] == '2' && buf[1] == '0' && buf[10] == 'T' && buf[19] == '.') { //looks like an ISO datetime at the front
+        jump_sz = jump_sz / 2;
+        buf[19] = '\0';
+        dt_str_cmp = strcmp(buf, filter_ts_start);
+        fpos += (dt_str_cmp <= 0) ? jump_sz : -jump_sz;
+        break;
+      }
+    }
+    if (fgres == NULL || line_ctr >= escape_line_no) {
+      fprintf(stderr, "Error: this file doesn't seem to have any ISO 8069 timestamps\n  (At least not in %u lines of one part)", escape_line_no);
+    }
+    fseek(fstr, fpos, SEEK_SET);
+  }
+  if (dt_str_cmp <= 0) { //if the last comparison was the line's ts > filter_ts_start, step back once
+    fpos -= jump_sz;
+    fseek(fstr, fpos, SEEK_SET);
+  }
+
+  //todo? finish seeking to the exact spot.
+
+  return;
+}
 
 void
 filter_lines(FILE* fstr, int verbose_flag, 
-    char* filter_ts_start, char* filter_ts_end,
-    char** filter_components, char** filter_threadname_prefixes, 
-    char* filter_severity_vals) {
+                  const char* filter_ts_start, const char* filter_ts_end,
+                  char** filter_components, char** filter_threadname_prefixes, 
+                  const char* filter_severity_vals) {
   int c;
   const size_t bufsize = 256 * 1024;
   char buf[bufsize];
@@ -60,14 +120,14 @@ filter_lines(FILE* fstr, int verbose_flag,
       }
 //printf("FIELDS: %s %s %s %s\n", ts, sev, cmpn, thrdnm);
 
-	  //todo optimization: once it starts, set true permanently, do any more strcmps
+      //todo optimization: once it starts, set true permanently, do any more strcmps
       if (!r && filter_ts_start && strcmp(filter_ts_start, ts) >= 0) {
         r = 1;
       }
 
-	  //todo optimization: once it becomes false exit func
+      //todo optimization: once it becomes false exit func
       if (!r && filter_ts_end && strcmp(filter_ts_end, ts) < 0) {
-        r = 1;
+        break;
       }
 
       if (!r && filter_components) {
@@ -136,6 +196,9 @@ int filter_file(const char* filepath, int verbose_flag,
 
   FILE* fstr;
   fstr = fopen(filepath, "r");
+  if (filter_ts_start) {
+    seek_start_ts(fstr, verbose_flag, filter_ts_start);
+  }
   filter_lines(fstr, verbose_flag, filter_ts_start, filter_ts_end,
                filter_components, filter_threadname_prefixes, filter_severity_vals);
   fclose(fstr);
