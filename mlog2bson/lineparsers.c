f73211e9475c3fc0a3fabbfe80a693cfa2dbce47
Akira Kurogane <akira.kurogane@10gen.com>
2016-10-26 14:28:22 +1100

    One example specialist parser func added.

--- mlog2bson/lineparsers.c
+++ mlog2bson/lineparsers.c
@@ -1,6 +1,32 @@
 #include "datemanip.h"
 #include "lineparsers.h"
 
+//"connection accepted from 10.10.2.211:48114 #1069642 (698 connections now open)"
+void lp_connection_accepted_from(bson_t* bson_doc, char* str, char** err_msg_ptr) {
+   char from_ip[13];
+   char from_port[7];
+   long conn_id;
+   long curr_incoming_conn_cnt;
+   char* strtol_endptr;
+   char* p = str + strlen("connection accepted from ");
+   char* e;
+   e = strchr(p, ':');
+   strncpy(from_ip, p, (e - p));
+   from_ip[e - p] = '\0';
+   p = e + 1;
+   e = strchr(p, ' ');
+   strncpy(from_port, p, (e - p));
+   from_port[e - p] = '\0';
+   p = e + 2;
+   conn_id = strtol(p, &strtol_endptr, 10);
+   p = strtol_endptr + 2; //mv past " ("
+   curr_incoming_conn_cnt = strtol(p, &strtol_endptr, 10);
+   bson_append_utf8(bson_doc, "ca", 2, from_ip, -1);
+   bson_append_utf8(bson_doc, "cap", 3, from_port, -1);
+   bson_append_int32(bson_doc, "i", 1, conn_id);
+   bson_append_int32(bson_doc, "iccnt", 5, curr_incoming_conn_cnt);
+}
+
 void parse_line_to_bson(bson_t* bson_doc, char* buf, size_t line_no, char** err_msg_ptr) {
 
     char* ts;
@@ -70,7 +96,11 @@ void parse_line_to_bson(bson_t* bson_doc, char* buf, size_t line_no, char** err_
           bson_append_int32(bson_doc, "i", 1, conn_id);
         }
 
-        bson_append_utf8(bson_doc, "x", 1, msg, -1);
+        if (strncmp(msg, "connection accepted from ", strlen("connection accepted from ")) == 0) {
+          lp_connection_accepted_from(bson_doc, msg, err_msg_ptr);
+        } else {
+          bson_append_utf8(bson_doc, "x", 1, msg, -1);
+        }
 
       } else {
         bson_append_bool(bson_doc, "not_mongo_log", 13, 1);
